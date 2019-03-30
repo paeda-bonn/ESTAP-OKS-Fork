503ddbff0a8f6c6feb1e1ebb9c7c60d13839d895
Nils Witt <nils@nils-witt.de>
2019-03-30 16:38:34 +0100

    Updated MySql to Fork Version

--- database/mysql.sql
+++ database/mysql.sql
@@ -1,72 +1,69 @@
---
--- Copyright 2013 Amos-Comenius-Gymnasium Bonn <http://www.acg-bonn.de/>
--- See LICENSE.md for licensing information. 
---
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS `appointments`;
-DROP TABLE IF EXISTS `time_slots`;
-DROP TABLE IF EXISTS `teachers`;
-DROP TABLE IF EXISTS `pupils`;
-DROP TABLE IF EXISTS `admins`;
-
-CREATE TABLE `pupils`
-(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `login` varchar(64) NOT NULL,
-    `password` varchar(128) NOT NULL,
-    `first_name` varchar(32) NOT NULL,
-    `last_name` varchar(32) NOT NULL,
-    `class` varchar(4) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `login` (`login`)
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(64) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `teachers`
-(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `login` varchar(64) NOT NULL UNIQUE,
-    `password` varchar(128) NOT NULL,
-    `first_name` varchar(32) NOT NULL,
-    `last_name` varchar(32) NOT NULL,
-    `gender` enum('f', 'm') NOT NULL,
-    `room` varchar(32) NOT NULL,
-    `active` tinyint(1) NOT NULL DEFAULT '1',
-    PRIMARY KEY (`id`)
+INSERT INTO `admins` (`id`, `login`, `password`, `first_name`, `last_name`) VALUES
+(1, 'admin', '$6$5249e7e24ba8c$6EHRYK58Xxo8ro3LJfhFu8QTzOwq//cX/FvVIwBIrEj/d58ZFukbj//Ul8xByXWfweYxBd4nR8fOWk98DlqxH.', 'First Name', 'Last Name'),
+
+CREATE TABLE IF NOT EXISTS `appointments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time_slot_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `pupil_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `appointments_teacher_time_slot` (`teacher_id`,`time_slot_id`),
+  UNIQUE KEY `appointments_teacher_pupil` (`teacher_id`,`pupil_id`),
+  KEY `appointments_time_slot` (`time_slot_id`),
+  KEY `appointments_pupil` (`pupil_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `admins`
-(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `login` varchar(64) NOT NULL UNIQUE,
-    `password` varchar(128) NOT NULL,
-    `first_name` varchar(32) NOT NULL,
-    `last_name` varchar(32) NOT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `pupils` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(64) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `class` varchar(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `time_slots`
-(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `start_time` time NOT NULL,
-    `end_time` time NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `start_time` (`start_time`)
+CREATE TABLE IF NOT EXISTS `teachers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(64) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `gender` enum('f','m') NOT NULL,
+  `room` varchar(32) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `appointments`(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `time_slot_id` int(11) NOT NULL,
-    `teacher_id` int(11) NOT NULL,
-    `pupil_id` int(11) NULL,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `appointments_time_slot` FOREIGN KEY (`time_slot_id`) 
-        REFERENCES `time_slots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `appointments_teacher` FOREIGN KEY (teacher_id) 
-        REFERENCES `teachers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `appointments_pupil`FOREIGN KEY (pupil_id) 
-        REFERENCES `pupils` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `appointments_teacher_pupil` UNIQUE (`teacher_id`, `pupil_id`),
-    CONSTRAINT `appointments_teacher_time_slot` UNIQUE (`teacher_id`, `time_slot_id`)
+CREATE TABLE IF NOT EXISTS `time_slots` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `date` date NOT NULL,
+  `Lehrer` text NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `admins` values (1, 'admin', '$6$5249e7e24ba8c$6EHRYK58Xxo8ro3LJfhFu8QTzOwq//cX/FvVIwBIrEj/d58ZFukbj//Ul8xByXWfweYxBd4nR8fOWk98DlqxH.', 'First Name', 'Last Name');
+
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `appointments_pupil` FOREIGN KEY (`pupil_id`) REFERENCES `pupils` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `appointments_teacher` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `appointments_time_slot` FOREIGN KEY (`time_slot_id`) REFERENCES `time_slots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
