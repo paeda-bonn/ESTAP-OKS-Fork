1842ecf680770dd4eb16171f57d67d751c3562a9
Nils Witt <nils@nils-witt.de>
2019-06-20 10:41:54 +0200

    Added option save & exit New Form layout
    Signed-off-by: Nils Witt <nils@nils-witt.de>

--- web/editTeacher.php
+++ web/editTeacher.php
@@ -78,104 +78,8 @@ else
       <?php h::bindField("room") ?>
       <label <?php h::label() ?>><?php h::msg("editTeacher.room") ?></label>
       <input type="text" <?php h::input() ?> <?php h::classes() ?> />
-      <?php h::messages();
-      h::bindField("times") 
-      ?>
-      <label <?php h::label() ?>><?php h::msg("editTeacher.times") ?></label>
-      <?php if($admin){ ?>
-        <a href="<?php h::url('deleteTimeSlotsTeacher.php?admin=1&teacher='.$teacherId) ?>">
-        <?php }else{ ?>
-          <a href="<?php h::url('deleteTimeSlotsTeacher.php?teacher='.$teacherId) ?>">
-        <?php } ?>
 
-      
 
-        <?php h::msg("editTeacher.deleteTimeSlots") ?></a>
-         <div class="fields">
-          <div class="field">
-            <?php h::bindField("duration") ?>
-            <label <?php h::label() ?>><?php h::msg("timeSlots.duration") ?></label>
-            <select <?php h::select() ?>>
-              <?php h::options(TimeSlot::getDurations()) ?>
-            </select>
-            <?php h::messages() ?>
-          </div>
-    
-          <div class="field">
-            <?php h::bindField("startHour") ?>
-            <label <?php h::label() ?>><?php h::msg("timeSlots.startTime") ?></label>
-            <select <?php h::select() ?>>
-              <?php h::options(TimeSlot::getHours()) ?>
-            </select>
-            <?php h::bindField("startMinute") ?>
-            <select <?php h::select() ?>>
-              <?php h::options(TimeSlot::getMinutes()) ?>
-            </select>
-            <?php h::bindField("startHour") ?>
-            <?php h::messages() ?>
-            <?php h::bindField("startMinute") ?>
-            <?php h::messages() ?>
-          </div>
-    
-          <div class="field">
-            <?php h::bindField("endHour") ?>
-            <label <?php h::label() ?>><?php h::msg("timeSlots.endTime") ?></label>
-            <select <?php h::select() ?>>
-              <?php h::options(TimeSlot::getHours()) ?>
-            </select>
-            <?php h::bindField("endMinute") ?>
-            <select <?php h::select() ?>>
-              <?php h::options(TimeSlot::getMinutes()) ?>
-            </select>
-            <?php h::bindField("endHour") ?>
-            <?php h::messages() ?>
-            <?php h::bindField("endMinute") ?>
-            <?php h::messages() ?>
-          </div>
-          <div class="field">
-                    <?php h::bindField("day") ?>
-                    <label <?php h::label() ?>><?php h::msg("timeSlots.day") ?></label>
-                    <select <?php h::select() ?>>
-                        <?php h::options(TimeSlot::getDays()) ?>
-                    </select>
-                    <?php h::bindField("month") ?>
-                    <select <?php h::select() ?>>
-                        <?php h::options(TimeSlot::getMonths()) ?>
-                    </select>
-                    <?php h::bindField("year") ?>
-                    <select <?php h::select() ?>>
-                        <?php h::options(TimeSlot::getYears()) ?>
-                    </select>
-                    <?php h::bindField("day") ?>
-                    <?php h::messages() ?>
-                    <?php h::bindField("month") ?>
-                    <?php h::messages() ?>
-                    <?php h::bindField("year") ?>
-                    <?php h::messages() ?>
-                </div>
-        </div>
-        <table>
-          <tr>
-            <th><?php h::msg("timeSlots.startTime") ?></th>
-            <th><?php h::msg("timeSlots.endTime") ?></th>
-            <th class="buttons"></th>
-          </tr>
-            <?php foreach (TimeSlot::getTimeSlotsForTeacher($teacherId) as $timeSlot): ?>
-                <tr>
-                    <td><?php h::text($timeSlot->getStartTimeString()) ?></td>
-                    <td><?php h::text($timeSlot->getEndTimeString()) ?></td>
-                    <td class="buttons">
-						<?php if($admin){ ?>
-						<a href="<?php h::url("deleteTimeSlotTeacher.php?admin&id=" . $timeSlot->getId()) ?>">
-						<?php }else{ ?>
-						<a href="<?php h::url("deleteTimeSlotTeacher.php?id=" . $timeSlot->getId()) ?>">
-						<?php } ?>
-                            <?php h::msg("timeSlots.delete") ?>
-                        </a>
-                    </td>
-                </tr>
-            <?php endforeach ?>
-        </table>
       <?php h::bindField("password") ?>
       <label <?php h::label() ?>><?php h::msg("editTeacher.password", Config::get()->getMinPasswordLength()) ?></label>
       <input type="password" <?=h::input() ?> <?php h::classes() ?> />
@@ -188,19 +92,127 @@ else
       
     </div>
 
-    <div class="buttons">
-      <input type="submit" value="<?php h::msg("editTeacher.submit") ?>" />
-      <?php if ($admin): ?>
-        <a href="<?php h::url("teachers.php") ?>">
-          <?php h::msg("editTeacher.cancel") ?>
-        </a>
-      <?php else: ?>  
-        <a href="<?php h::url("teacherAppointments.php") ?>">
-          <?php h::msg("editTeacher.cancel") ?>
-        </a>
-      <?php endif ?>
-    </div>  
-  </form>
 
-</div> 
+        <div class="fields">
+            <div class="field">
+                <?php h::bindField("duration") ?>
+                <label <?php h::label() ?>><?php h::msg("timeSlots.duration") ?></label>
+                <select <?php h::select() ?>>
+                    <?php h::options(TimeSlot::getDurations()) ?>
+                    <?php print_r(TimeSlot::getDurations()) ?>
+                </select>
+                <?php h::messages() ?>
+            </div>
+
+            <div class="field">
+                <?php h::bindField("startHour") ?>
+                <label <?php h::label() ?>><?php h::msg("timeSlots.startTime") ?></label>
+                <select <?php h::select() ?>>
+                    <?php h::options(TimeSlot::getHours()) ?>
+                </select>
+                <?php h::bindField("startMinute") ?>
+                <select <?php h::select() ?>>
+                    <?php h::options(TimeSlot::getMinutes()) ?>
+                </select>
+                <?php h::bindField("startHour") ?>
+                <?php h::messages() ?>
+                <?php h::bindField("startMinute") ?>
+                <?php h::messages() ?>
+            </div>
+
+            <div class="field">
+                <?php h::bindField("endHour") ?>
+                <label <?php h::label() ?>><?php h::msg("timeSlots.endTime") ?></label>
+                <select <?php h::select() ?>>
+                    <?php h::options(TimeSlot::getHours()) ?>
+                </select>
+                <?php h::bindField("endMinute") ?>
+                <select <?php h::select() ?>>
+                    <?php h::options(TimeSlot::getMinutes()) ?>
+                </select>
+                <?php h::bindField("endHour") ?>
+                <?php h::messages() ?>
+                <?php h::bindField("endMinute") ?>
+                <?php h::messages() ?>
+            </div>
+            <div class="field">
+                <?php h::bindField("day") ?>
+                <label <?php h::label() ?>><?php h::msg("timeSlots.day") ?></label>
+                <select <?php h::select() ?>>
+                    <?php h::options(TimeSlot::getDays()) ?>
+                </select>
+                <?php h::bindField("month") ?>
+                <select <?php h::select() ?>>
+                    <?php h::options(TimeSlot::getMonths()) ?>
+                </select>
+                <?php h::bindField("year") ?>
+                <select <?php h::select() ?>>
+                    <?php h::options(TimeSlot::getYears()) ?>
+                </select>
+                <?php h::bindField("day") ?>
+                <?php h::messages() ?>
+                <?php h::bindField("month") ?>
+                <?php h::messages() ?>
+                <?php h::bindField("year") ?>
+                <?php h::messages() ?>
+            </div>
+
+
+                <?php h::messages();
+                h::bindField("times")
+                ?>
+                <label <?php h::label() ?>><?php h::msg("editTeacher.times") ?></label>
+                <?php if($admin){ ?>
+                <a href="<?php h::url('deleteTimeSlotsTeacher.php?admin=1&teacher='.$teacherId) ?>">
+                    <?php }else{ ?>
+                    <a href="<?php h::url('deleteTimeSlotsTeacher.php?teacher='.$teacherId) ?>">
+                        <?php } ?>
+
+
+
+                        <?php h::msg("editTeacher.deleteTimeSlots") ?></a>
+        </div>
+        <table>
+            <?php $i = 0; ?>
+            <?php foreach (TimeSlot::getTimeSlotsForTeacher($teacherId) as $timeSlot): ?>
+                <?php if($i == 0): ?>
+                    <tr>
+                        <th><?php h::msg("timeSlots.startTime") ?></th>
+                        <th><?php h::msg("timeSlots.endTime") ?></th>
+                        <th class="buttons"></th>
+                    </tr>
+                    <?php $i = 1; ?>
+                <?php endif; ?>
+                <tr>
+                    <td><?php h::text($timeSlot->getStartTimeString()) ?></td>
+                    <td><?php h::text($timeSlot->getEndTimeString()) ?></td>
+                    <td class="buttons">
+                        <?php if($admin){ ?>
+                        <a href="<?php h::url("deleteTimeSlotTeacher.php?admin&id=" . $timeSlot->getId().'&teacher='.$teacherId) ?>">
+                            <?php }else{ ?>
+                            <a href="<?php h::url("deleteTimeSlotTeacher.php?id=" . $timeSlot->getId()) ?>">
+                                <?php } ?>
+                                <?php h::msg("timeSlots.delete") ?>
+                            </a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+        </table>
+        <div class="buttons">
+            <input type="submit" name="leave" value="<?php h::msg("editTeacher.submitLeave") ?>" />
+            <input type="submit" name="return" value="<?php h::msg("editTeacher.submitStay") ?>" />
+            <?php if ($admin): ?>
+                <a href="<?php h::url("teachers.php") ?>">
+                    <?php h::msg("editTeacher.cancel") ?>
+                </a>
+            <?php else: ?>
+                <a href="<?php h::url("teacherAppointments.php") ?>">
+                    <?php h::msg("editTeacher.cancel") ?>
+                </a>
+            <?php endif ?>
+        </div>
+    </div>
+</form>
+
+
 <?php include "parts/footer.php" ?>
